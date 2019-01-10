8b4657b2c066d692c5efdef5efde1f4ffcb13cca
JAMES LEE SU KEY <james.l.s.k0902@hotmail.com>
2019-01-10 13:28:11 +0800

    change to another type of pinger

--- check.js
+++ check.js
@@ -1,90 +1,119 @@
 var nodemailer = require('nodemailer'); 
-const isReachable = require('is-reachable');
+var fetch = require('node-fetch');
+var outlet = require('./data.js');
+
 
 var count1 = 0;
-var sent = true;
+var sent1 = false;
+var sent2 = false;
+var sent3 = false;
+var sent4 = false;
 
 var transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
         auth:{
-                user: 'ptutm.jameslsk@gmail.com',
-                pass: 'james009'
+                user: outlet.email,
+                pass: outlet.gmailpw
         }
 });
 
-function checkPJ21 () {
-	isReachable('https://rugulose-akita-0733.dataplicity.io/').then(function(reachable){
-		console.log(reachable);
-		if (reachable == false) {
-			count1++;
-			if (count <= 10) {
+
+function pingSP() {
+	var data = {"Online": "Yes"};
+	fetch('https://aortic-sheep-0060.dataplicity.io/pingme',
+		{headers: {'Content-Type': 'application/json'},
+		method:'POST',
+		body:JSON.stringify(data)
+	}).then(res => res.json())
+	.catch(error => {
+		console.error('Error:', error)
+		console.log("SP Server is Offline")
+		if(!sent1) {
+			var mailOptions = {
+                        	from: outlet.email,
+                                to: 'jamesleesukey@gmail.com',
+                                subject: 'SP server is Offline',
+                                text: "Server status sent by Pinger"
+                        };
+                        transporter.sendMail(mailOptions, function(error, info){
+                                if (error) {
+                                        console.log(error);
+                                } else {
+                                        console.log('Email sent: ' + info.response);
+                                }
+                        });y
+                        sent = true;
+		}
+
+	})
+	.then(response => {
+		//console.log('Success:', response)
+		var status = response.status
+		if (status == "Online") {
+			console.log("SP Server is online")
+			if (sent1) {
 				var mailOptions = {
-		        		from: 'ptutm.jameslsk@gmail.com',
-        				to: 'jamesleesukey@gmail.com',
-        				subject: 'Sending Email to notify that the PJ21 server is unreachable at the moment',
-        				text: "the server has been down for " + count1
+					from: outlet.email,
+					to: 'jamesleesukey@gmail.com',
+					subject: 'SP server is online',
+					text: "Server status sent by Pinger"
 				};
 				transporter.sendMail(mailOptions, function(error, info){
-        	        		if (error) {
-        	        			console.log(error);
-        	               		} else {
-        	                	       console.log('Email sent: ' + info.response);
-        	              		}
-        	       		});
-			}
-			sent = false;
-		} else if (reachable == true) {
-			if (sent == true) {
-			} else {
-				var mailOptions = {
-        	                	from: 'ptutm.jameslsk@gmail.com',
-        	                	to: 'jamesleesukey@gmail.com',
-        	                	subject: 'Sending Email to notify that PJ21 server has back to normal condition',
-        	                	text: "the server is up now"
-        	        	};
-        	        	transporter.sendMail(mailOptions, function(error, info){
-        	                	if (error) {
-        	                        	console.log(error);
-        	                	} else {
-        	                       		console.log('Email sent: ' + info.response);
-        	                	}
-        	        	});
-				sent = true;
+					if (error) {
+						console.log(error);
+					} else {
+						console.log('Email sent: ' + info.response);
+					}
+				});
+				sent = false;
 			}
 		}
-		//=> true
+		//myRefreshToken = response.refreshToken
 	})
 }
 
-function checkPJCC () {
-        isReachable('https://rugulose-akita-0733.dataplicity.io/').then(function(reachable){
-                console.log(reachable);
-                if (reachable == false) {
-                        count1++;
-                        if (count <= 10) {
+
+function pingPJCC() {
+        var data = {"Online": "Yes"};
+        fetch('https://underfired-ibis-3986.dataplicity.io/pingme',
+                {headers: {'Content-Type': 'application/json'},
+                method:'POST',
+                body:JSON.stringify(data)
+        }).then(res => res.json())
+        .catch(error => {
+                console.error('Error:', error)
+                console.log("PJCC Server is Offline")
+		if(!sent2) {
+                        var mailOptions = {
+                                from: outlet.email,
+                                to: 'jamesleesukey@gmail.com',
+                                subject: 'PJCC server is Offline',
+                                text: "Server status sent by Pinger"
+                        };
+                        transporter.sendMail(mailOptions, function(error, info){
+                                if (error) {
+                                        console.log(error);
+                                } else {
+                                        console.log('Email sent: ' + info.response);
+                                }
+                        });
+                        sent = true;
+                }
+
+        })
+        .then(response => {
+                //console.log('Success:', response)
+                var status = response.status
+                if (status == "Online") {
+                        console.log("PJCC Server is online")
+                	if (sent2) {
                                 var mailOptions = {
-                                        from: 'ptutm.jameslsk@gmail.com',
+                                        from: outlet.email,
                                         to: 'jamesleesukey@gmail.com',
-                                        subject: 'Sending Email to notify that the PJCC server is unreachable at the moment',
-                                        text: "the server has been down for " + count1
-                                };
-                                transporter.sendMail(mailOptions, function(error, info){
-                                        if (error) {
-                                                console.log(error);
-                                        } else {
-                                               console.log('Email sent: ' + info.response);
-                                        }
-                                });
-                        }
-                        sent = false;
-                } else if (reachable == true) {
-                        if (sent == true) {
-                        } else {
-                                var mailOptions = {
-                                        from: 'ptutm.jameslsk@gmail.com',
-                                        to: 'jamesleesukey@gmail.com',
-                                        subject: 'Sending Email to notify that PJCC server has back to normal condition',
-                                        text: "the server is up now"
+                                        subject: 'PJCC server is online',
+                                        text: "Server status sent by Pinger"
                                 };
                                 transporter.sendMail(mailOptions, function(error, info){
                                         if (error) {
@@ -93,18 +122,182 @@ function checkPJCC () {
                                                 console.log('Email sent: ' + info.response);
                                         }
                                 });
-                                sent = true;
+                                sent = false;
                         }
-                }
-                //=> true
+
+		}
+                //myRefreshToken = response.refreshToken
         })
 }
 
 
-setInterval(checkPJCC, 60000);
-setInterval(checkPJ21, 60000);
+function pingPJ21() {
+        var data = {"Online": "Yes"};
+        fetch('https://membranous-molly-7085.dataplicity.io/pingme',
+                {headers: {'Content-Type': 'application/json'},
+                method:'POST',
+                body:JSON.stringify(data)
+        }).then(res => res.json())
+        .catch(error => {
+                console.error('Error:', error)
+                console.log("PJ21 Server is Offline")
+		 if(!sent3) {
+                        var mailOptions = {
+                                from: outlet.email,
+                                to: 'jamesleesukey@gmail.com',
+                                subject: 'PJ21 server is Offline',
+                                text: "Server status sent by Pinger"
+                        };
+                        transporter.sendMail(mailOptions, function(error, info){
+                                if (error) {
+                                        console.log(error);
+                                } else {
+                                        console.log('Email sent: ' + info.response);
+                                }
+                        });
+                        sent = true;
+                }
 
-//isReachable('google.com:80').then(reachable => {
-//	console.log(reachable);
-	//=> true
-//});
+        })
+        .then(response => {
+                //console.log('Success:', response)
+                var status = response.status
+                if (status == "Online") {
+                        console.log("PJ21 Server is online")
+                	if (sent3) {
+                                var mailOptions = {
+                                        from: outlet.email,
+                                        to: 'jamesleesukey@gmail.com',
+                                        subject: 'PJ21 server is online',
+                                        text: "Server status sent by Pinger"
+                                };
+                                transporter.sendMail(mailOptions, function(error, info){
+                                        if (error) {
+                                                console.log(error);
+                                        } else {
+                                                console.log('Email sent: ' + info.response);
+                                        }
+                                });
+                                sent = false;
+                        }
+
+		}
+                //myRefreshToken = response.refreshToken
+        })
+}
+
+function pingBBB() {
+        var data = {"Online": "Yes"};
+        fetch('https://coronate-pademelon-4727.dataplicity.io/pingme',
+                {headers: {'Content-Type': 'application/json'},
+                method:'POST',
+                body:JSON.stringify(data)
+        }).then(res => res.json())
+        .catch(error => {
+                console.error('Error:', error)
+                console.log("BBB Server is Offline")
+		if(!sent4) {
+                        var mailOptions = {
+                                from: outlet.email,
+                                to: 'jamesleesukey@gmail.com',
+                                subject: 'BBB server is Offline',
+                                text: "Server status sent by Pinger"
+                        };
+                        transporter.sendMail(mailOptions, function(error, info){
+                                if (error) {
+                                        console.log(error);
+                                } else {
+                                        console.log('Email sent: ' + info.response);
+                                }
+                        });
+                        sent = true;
+                }
+        })
+        .then(response => {
+                //console.log('Success:', response)
+                var status = response.status
+                if (status == "Online") {
+                        console.log("BBB Server is online")
+                	if (sent4) {
+                                var mailOptions = {
+                                        from: outlet.email,
+                                        to: 'jamesleesukey@gmail.com',
+                                        subject: 'BBB server is online',
+                                        text: "Server status sent by Pinger"
+                                };
+                                transporter.sendMail(mailOptions, function(error, info){
+                                        if (error) {
+                                                console.log(error);
+                                        } else {
+                                                console.log('Email sent: ' + info.response);
+                                        }
+                                });
+                                sent = false;
+                        }
+		}
+                //myRefreshToken = response.refreshToken
+        })
+}
+
+function pingTest() {
+        var data = {"Online": "Yes"};
+        fetch('https://https://overshot-wolf-2024.dataplicity.io/pingme',
+                {headers: {'Content-Type': 'application/json'},
+                method:'POST',
+                body:JSON.stringify(data)
+        }).then(res => res.json())
+        .catch(error => {
+                console.error('Error:', error)
+                console.log("Test Server is Offline")
+                if(!sent4) {
+                        var mailOptions = {
+                                from: outlet.email,
+                                to: 'jamesleesukey@gmail.com',
+                                subject: 'Test server is Offline',
+                                text: "Server status sent by Pinger"
+                        };
+                        transporter.sendMail(mailOptions, function(error, info){
+                                if (error) {
+                                        console.log(error);
+                                } else {
+                                        console.log('Email sent: ' + info.response);
+                                }
+                        });
+                        sent = true;
+                }
+        })
+        .then(response => {
+                //console.log('Success:', response)
+                var status = response.status
+                if (status == "Online") {
+                        console.log("Test Server is online")
+                        if (sent4) {
+                                var mailOptions = {
+                                        from: outlet.email,
+                                        to: 'jamesleesukey@gmail.com',
+                                        subject: 'Test server is online',
+                                        text: "Server status sent by Pinger"
+                                };
+                                transporter.sendMail(mailOptions, function(error, info){
+                                        if (error) {
+                                                console.log(error);
+                                        } else {
+                                                console.log('Email sent: ' + info.response);
+                                        }
+                                });
+                                sent = false;
+                        }
+                }
+                //myRefreshToken = response.refreshToken
+        })
+}
+
+
+
+setInterval(function() {
+	pingSP();
+	pingPJCC();
+	pingPJ21();
+	pingBBB();
+	pingTest();
+}, 600000)
---
+++ data.js
@@ -0,0 +1,4 @@
+module.exports = {
+	email: "ptutm.jameslsk@gmail.com",
+	gmailpw: "Callmekoka@008"
+}
